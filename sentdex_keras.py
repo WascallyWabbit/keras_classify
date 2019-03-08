d54607560a9dd8f14acd9b017ee3745d4568c09a
Eric Fowler <eric.fowler@gmail.com>
2019-03-07 22:30:23 -0800

    first commit

---
+++ sentdex_keras.py
@@ -0,0 +1,80 @@
+import tensorflow as tf
+from time import time
+from tensorflow.python.keras.callbacks import TensorBoard
+import random
+import numpy as np
+import utilities as ut
+import matplotlib.pyplot as plt
+import datetime
+#file_path_carvana_test = '/Users/Eric Fowler/Downloads/carvana/test/'
+file_path_carvana_train = '/Users/Eric Fowler/Downloads/carvana/train/'
+
+
+def main():
+    FLAGS, unparsed = ut.parseArgs()
+    print(FLAGS)
+    # TEST_DATA_PATH      = FLAGS.test_data_path
+    SAMPLE_FILE = FLAGS.train_data_path + FLAGS.sample + '.' + FLAGS.img_file_extension
+
+    img = ut.read_image(filename=SAMPLE_FILE, show=False)
+    img = np.array(img)
+    IMG_SHAPE=img.shape
+    (x_train, y_train), (x_test, y_test)=load_data(numclasses=FLAGS.numclasses, train_path=FLAGS.train_data_path, onehot=True)
+
+    print(f'IMG_SHAPE:{IMG_SHAPE},  y_train shape:{y_train[0].shape}')
+
+    model = tf.keras.models.Sequential(
+    [
+    tf.keras.layers.Conv2D(4,(8,8), strides=4, activation='relu',input_shape=IMG_SHAPE,batch_size=FLAGS.batch_size),
+    tf.keras.layers.MaxPool2D(),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(16, activation='softmax', name='softmax')])
+    print(f'Saving in {FLAGS.tb_dir+datetime.datetime.now().strftime("%Y%m%d%H%M%S")}')
+    tensorboard = TensorBoard(log_dir=FLAGS.tb_dir+f'{datetime.datetime.now().strftime("%Y%m%d%H%M%S")}')
+
+    optimizer=tf.keras.optimizers.Adam(lr=FLAGS.learning_rate)
+    model.compile(optimizer=optimizer,
+                  loss='mean_squared_error',
+                  metrics=['accuracy']
+                  )
+
+    model.summary()
+
+    for i in range(FLAGS.epochs):
+        print(f'Epoch:{i} of {FLAGS.epochs}')
+        n = len(x_train)
+        for batch in range(0,len(x_train), FLAGS.batch_size):
+            print(f'Batch {batch} of {n}.')
+            bunch_x, bunch_y = x_train[batch:batch+FLAGS.batch_size], y_train[batch:batch+FLAGS.batch_size]
+            if len(bunch_x) < FLAGS.batch_size: # skip partial batches
+                print(f'Skipping {len(bunch_x)} samples..')
+                continue
+
+            xs = []
+            ys = []
+            for datum in range(len(bunch_x)):
+                file = bunch_x[datum]
+                img = ut.read_image(filename=FLAGS.train_data_path+file, show=False)
+                img=np.array(img)
+                xs.append(img)
+                ys.append(bunch_y[datum])
+
+            X= np.stack(xs, axis=0)
+            Y= np.stack(ys, axis=0)
+            model.fit(x=X, y=Y,steps_per_epoch=10, callbacks=[tensorboard])
+
+            score = model.evaluate(x=X,y=Y, batch_size=FLAGS.batch_size)
+            print(f'Score:{score}')
+
+    pass
+
+def load_data(numclasses=16, train_path=file_path_carvana_train, num_images = None, onehot=False):
+    training_list = ut.get_tensor_list(num_classes=numclasses, path=train_path, num=num_images, onehot=onehot)
+    random.shuffle(training_list)
+    n=len(training_list)
+    tr = training_list[:7 * n // 8]
+    te = training_list[7 * n // 8:]
+    return ([pngs for (pngs,labels) in tr],[labels for (pngs,labels) in tr]),([pngs for (pngs,labels) in te],[labels for (pngs,labels) in te])
+
+if __name__ == '__main__':
+    main()
---
+++ utilities.py
@@ -0,0 +1,121 @@
+import argparse
+import os
+import random
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_data_path', type=str,
+                        default='/Users/Eric Fowler/Downloads/carvana/train/_128x128/',
+                        help='Directory for storing input data')
+    parser.add_argument('--test_data_path', type=str,
+                        default='/Users/Eric Fowler/Downloads/carvana/test/_128x128/',
+                        help='Directory for storing input data')
+    parser.add_argument('--target', type=str,
+                        default='mnist',
+                        choices=['carvana', 'mnist'],
+                        help='MNIST or Carvana?')
+    parser.add_argument('--sample', type=str,
+                        default='0cdf5b5d0ce1_01',
+                        help='Sample image file for sizing feature tensor')
+    parser.add_argument('--numclasses', type=int,
+                        default=16,
+                        help='Carvana=16, MNIST=10')
+    parser.add_argument('--learning_rate', type=float,
+                        default=0.001,
+                        help='Learning rate')
+    parser.add_argument('--show', type=bool,
+                         default=False,
+                         help='Show some images?')
+    parser.add_argument('--scale', type=float,
+                        default=1.0,
+                        help='Scaling factor for images')
+    parser.add_argument('--epochs', type=int,
+                        default=2,
+                        help='Epochs')
+    parser.add_argument('--batch_size', type=int,
+                        default=10,
+                        help='Cut samples into chunks of this size')
+    parser.add_argument('--tb_dir', type=str,
+                        default='./logs/',
+                        help='Directory For Tensorboard log')
+    parser.add_argument('--img_file_extension', type=str,
+                        default='png',
+                        help='Extension of image file names')
+
+    return parser.parse_known_args()
+
+
+def get_tensor_list(path, num_classes=16, num=None, onehot=False):
+    files = os.listdir(path)
+
+    if not files:
+        return None
+
+    pngs = [f for f in files if f.endswith('png')]  # this gets 'filename_37.png'
+
+    number_in_filename = [name_fragment.split('_')[1] for name_fragment in pngs]  # this gets '37.png'
+    number_in_filename = [name_fragment.split('.')[0] for name_fragment in number_in_filename]  # this gets '37'
+    label_array = np.asarray(number_in_filename, dtype=np.int32) - 1
+    if onehot == True:
+        labels = np.zeros((len(label_array), num_classes), dtype=np.float32)
+        labels[np.arange(len(label_array)), label_array] = 1.
+    else:
+        labels = label_array
+
+    if num is None:
+        num = len(pngs)
+
+    # return must be list of tuples (filename, label array [one-hot bool])
+    ret = list(zip(pngs[:num], labels[:num]))
+    random.shuffle(ret)
+    return ret
+
+def scale_image(img, scale=1.0):
+    if scale != 1.0:
+        s = img.size
+        img=img.resize((int(s[0] // scale), int(s[1] // scale)))
+    return img
+
+def flatten_image(img):
+    img = img.flatten('F')
+    return img
+
+def get_image_shape(filename, scale, show=False):
+    img = read_image(filename=filename, show=show, scale=scale)
+    return img.size
+
+def pixnum_from_img_shape(img_shape):
+    pixel_num = 1
+    for t in img_shape:
+        pixel_num *= t
+
+    return pixel_num
+
+
+def read_image(filename, show, scale=1.0):
+   mm = Image.open(filename).convert('LA')
+   mm = scale_image(img=mm, scale=scale)
+
+   if show == True:
+       plt.imshow(mm)
+       plt.show()
+
+   return mm
+
+import itertools as it
+import sys
+
+if sys.version[0]=='2':
+    it.zip_longest=it.izip_longest
+
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    a = [iter(iterable)]*n
+    ret = it.zip_longest(*a, fillvalue=fillvalue)
+    args= [n for n in ret if n is not None]
+
+    return args
